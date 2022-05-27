4c9674d9ad3534d3efd94d97fe3bb796712fe42b
Dineshkumar-Anbalagan <dinesh2172@outlook.com>
2022-05-27 12:52:23 +0530

    leap year

---
+++ Day 3 of 100/LeapYear.py
@@ -0,0 +1,7 @@
+year = int(input("What year do you wanna check? "))
+
+if year%4 == 0 and (year%100 == 0 or year%400 == 0):
+    print(f"Year {year} is a leap year")
+else:
+    print(f"Year {year} is not a leap year")
+
